c61e6be0e8ee8ca2ae10102a424ae837920269e8
Amal Sunny <amalsunny992@gmail.com>
2024-08-05 06:17:18 +0530

    deleted old files

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const skills = document.querySelectorAll('.skill-level');
-    skills.forEach(skill => {
-        skill.style.width = '0';
-        setTimeout(() => {
-            skill.style.width = skill.getAttribute('style').split(':')[1];
-        }, 1000);
-    });
-});
